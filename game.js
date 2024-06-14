2aa304db6ab908529656b90621a40ad85b028226
justinnebaltazar <jfb3@sfu.ca>
2024-06-13 22:25:43 -0700

    Add a welcome message before the game begins

--- game.js
+++ game.js
@@ -1,41 +1,35 @@
-function getComputerChoice() {
-
-    let choice;
-    let number = Math.random();
-
-    if (number >= 0 && number < 0.33) {
-        choice = 'rock';
-    } else if (number >= 0.33 && number < 0.67) {
-        choice = 'scissors';
-    } else {
-        choice = 'paper';
-    }
-
-    return choice;
-};
-
-function getHumanChoice() {
-
-    let choice = prompt("Rock, Paper, or Scissors: ")
-    if (choice.toLowerCase() === "rock") {
-        choice = 'rock';
-    } else if (choice.toLowerCase() === "paper") {
-        choice = 'paper';
-    } else if (choice.toLowerCase() === "scissors") {
-        choice = 'scissors';
-    }
-
-    return choice;
-
-};
-
-const humanChoice = getHumanChoice(); 
-const computerChoice = getComputerChoice();
-
 function playGame() {
-
+    alert("Welcome to Rock-Paper-Scissors! The round's winner will be announced. After 5 rounds, a winner will be decided.")
+    
     let humanScore = 0;
     let computerScore = 0;
+
+    function getComputerChoice() {
+
+        let choice;
+        let number = Math.random();
+        if (number >= 0 && number < 0.33) {
+            choice = 'rock';
+        } else if (number >= 0.33 && number < 0.67) {
+            choice = 'scissors';
+        } else {
+            choice = 'paper';
+        }
+        return choice;
+    };
+    
+    function getHumanChoice() {
+    
+        let choice = prompt("Rock, Paper, or Scissors: ")
+        if (choice.toLowerCase() === "rock") {
+            choice = 'rock';
+        } else if (choice.toLowerCase() === "paper") {
+            choice = 'paper';
+        } else if (choice.toLowerCase() === "scissors") {
+            choice = 'scissors';
+        }
+        return choice;
+    };
 
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
